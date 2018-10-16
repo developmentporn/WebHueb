086e27641097f97c63b41b79e73475eee7792e63
Stanislav <kenny2lighter@gmail.com>
2018-10-16 23:37:05 +0300

    Universal data transfer object

---
+++ BoldyBuildhui/Models/RootDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BoldyBuildhui.Models
+{
+    public class RootDTO<T>
+    {
+        public string Type { get; set; }
+        public string Version { get; set; }
+        public Dictionary<string, T> Data { get; set; }
+    }
+}
